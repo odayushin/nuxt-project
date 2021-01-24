b45ed4c84ce88e1e55f5706994aaeba68882ab02
odayushin <oda.yushin@gmail.com>
2021-01-24 23:27:12 +0900

    first commit

---
+++ pages/fun.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <input v-model="message" placeholder="edit me" />
+    <p>Message is: {{ message }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <button v-on:click="turn">隠しエリア</button>
+    <div v-if="isMessageOk">隠しエリアだよー</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isMessageOk: true,
+    };
+  },
+  methods() {
+    return {
+      turn: function () {
+        this.isMessageOk = !this.isMessageOk;
+      },
+    };
+  },
+};
+</script>
