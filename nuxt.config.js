05e65d869162003a312f89b14f3b3224625a1227
odayushin <oda.yushin@gmail.com>
2021-02-22 15:55:17 +0900

    nuxt.config.jsでssrをfalseにした

---
+++ nuxt.config.js
@@ -0,0 +1,23 @@
+export default {
+    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+    ssr: false,
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+    ],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+    ],
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    }
+}
